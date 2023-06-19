a06a665c1734510b0be05ec2047c7c425e50be40
Julian Schumacher <professional@julianschumacher.dev>
2023-06-19 20:36:08 +0200

    Update code

--- php/unterricht2/abc.php
+++ php/unterricht2/abc.php
@@ -16,9 +16,13 @@
         */
 
         // Wertzuweisung
-        $a = 1;
-        $b = 3; 
-        $c = 2;
+        $a = $_GET['a'];
+        $b = $_GET['b']; 
+        $c = $_GET['c'];
+
+        $start = $_GET['start'];
+        $end = $_GET['end'];
+        $steps = $_GET['steps'];
 
         $radikand = $b*$b - 4*$a*$c;
         
@@ -33,11 +37,38 @@
 
         $x2 = (-$b - sqrt($radikand)) / (2*$a);
 
-        // Hier wird das Ergebnis ausgegeben
-        echo "<strong>1. Nullstelle:</strong> $x1";
-        echo "<br>";
-        echo "<strong>2. Nullstelle</strong>  $x2";
+        if (is_nan($x1) || is_nan($x2)) {
+            $nullstelle;
+            if (is_nan($x1) && is_nan($x2)) {
+                echo "<strong>Keine Nullstelle</strong>";
+            } else {
+            if (is_nan($x1)) {
+                $nullstelle = $x2;
+            } else {
+                $nullstelle = $x1;
+            }
+            echo "<strong>Eine Nullstelle $nullstelle</strong>";
+        }
+        } else {
+            echo "<strong>1. Nullstelle:</strong> $x1";
+            echo "<br>";
+            echo "<strong>2. Nullstelle</strong>  $x2";
+        }
 
+        echo "<h1>Wertetabelle</h1>";
+        echo "<table>";
+        echo "<tr>";
+        for ($x = $start; $x < $end; $x = $x + $steps) {
+            echo "<td>$x</td>";
+        }
+        echo "</tr>";
+        echo "<tr>";
+        for ($x = $start; $x < $end; $x = $x + $steps) {
+            $result = ($a * pow($x, 2)) + ($b * $x) + ($c);
+            echo "<td>$result</td>";
+        }
+        echo "</tr>";
+        echo "</table>";
     ?>
 </body>
 
